50772b1e646d2dde0d1766e55709b51889cebd34
lgraesser <lhgraesser@gmail.com>
2018-08-19 22:39:52 -0700

    Adding 3 x compexity measures to cross product analyzer

--- logs/analyze_xproduct.sh
+++ logs/analyze_xproduct.sh
@@ -1,20 +1,77 @@
 #!/bin/bash
 # Outputs a 2d table containing the in domain accuracy for all possible pairs of agents
 file="$1.log"
-output="$1_xproduct.csv"
+output="$1_xproduct"
 num=$[$2+1]
 echo "Analyzing $file"
 
-cat $file | grep "In Domain Agents 1,[0-9][0-9]* Development Accuracy, both right, after comms:" | sed 's/.*Agents [0-9][0-9]*,//g' | sed 's/\([0-9][0-9]*\).*comms: /\1,/g' > $output
+echo "Calculating accuracy for xproduct"
+cat $file | grep "In Domain Agents 1,[0-9][0-9]* Development Accuracy, both right, after comms:" | sed 's/.*Agents [0-9][0-9]*,//g' | sed 's/\([0-9][0-9]*\).*comms: /\1,/g' > "${output}_accuracy.csv"
+# cat "${output}_accuracy.csv"
 
 anum=2
 while [ $anum -lt $num ]
 do
   # cat $file | grep "In Domain Agents $anum,[0-9][0-9]* Development Accuracy, both right, after comms:"
   cat $file | grep "In Domain Agents $anum,[0-9][0-9]* Development Accuracy, both right, after comms:" | sed 's/.*Agents [0-9][0-9]*,//g' | sed 's/\([0-9][0-9]*\).*comms: /\1,/g' > temp.txt
-  join -t , $output temp.txt > tmp && mv tmp $output
+  join -t , "${output}_accuracy.csv" temp.txt > tmp && mv tmp "${output}_accuracy.csv"
   anum=$[$anum+1]
+  # cat "${output}_accuracy.csv"
 done
 
-cat $output
+cat "${output}_accuracy.csv"
+rm temp.txt
+
+echo
+echo "Calculating E[H(m|x)] for xproduct"
+cat $file | grep "In Domain Agents 1,[0-9][0-9]*: complexity report: total:" | sed 's/.*Agents [0-9][0-9]*,//g' | sed 's/\([0-9][0-9]*\).*avg ent: \([0-9][0-9]*.[0-9][0-9]*\), ent_avg_msg.*/\1,\2/g' > "${output}_avgent.csv"
+# cat "${output}_avgent.csv"
+
+anum=2
+while [ $anum -lt $num ]
+do
+  cat $file | grep "In Domain Agents $anum,[0-9][0-9]*: complexity report: total:" | sed 's/.*Agents [0-9][0-9]*,//g' | sed 's/\([0-9][0-9]*\).*avg ent: \([0-9][0-9]*.[0-9][0-9]*\), ent_avg_msg.*/\1,\2/g' > temp.txt
+  join -t , "${output}_avgent.csv" temp.txt > tmp && mv tmp "${output}_avgent.csv"
+  anum=$[$anum+1]
+  # cat temp.txt
+  # cat "${output}_avgent.csv"
+done
+
+cat "${output}_avgent.csv"
+rm temp.txt
+
+echo
+echo "Calculating H(E[m|x]) for xproduct"
+cat $file | grep "In Domain Agents 1,[0-9][0-9]*: complexity report: total:" | sed 's/.*Agents [0-9][0-9]*,//g' | sed 's/\([0-9][0-9]*\).*ent_avg_msg: \([0-9][0-9]*.[0-9][0-9]*\), num_msgs.*/\1,\2/g' > "${output}_entavgmsg.csv"
+# cat "${output}_entavgmsg.csv"
+
+anum=2
+while [ $anum -lt $num ]
+do
+  cat $file | grep "In Domain Agents $anum,[0-9][0-9]*: complexity report: total:" | sed 's/.*Agents [0-9][0-9]*,//g' | sed 's/\([0-9][0-9]*\).*ent_avg_msg: \([0-9][0-9]*.[0-9][0-9]*\), num_msgs.*/\1,\2/g' > temp.txt
+  join -t , "${output}_entavgmsg.csv" temp.txt > tmp && mv tmp "${output}_entavgmsg.csv"
+  anum=$[$anum+1]
+  # cat temp.txt
+  # cat "${output}_entavgmsg.csv"
+done
+
+cat "${output}_entavgmsg.csv"
+rm temp.txt
+
+echo
+echo "Calculating number of distinct messages"
+cat $file | grep "In Domain Agents 1,[0-9][0-9]*: complexity report: total:" | sed 's/.*Agents [0-9][0-9]*,//g' | sed 's/\([0-9][0-9]*\).*distinct msg: \([0-9][0-9]]*\)/\1,\2/g' > "${output}_entavgmsg.csv"
+# cat "${output}_entavgmsg.csv"
+
+anum=2
+while [ $anum -lt $num ]
+do
+  cat $file | grep "In Domain Agents $anum,[0-9][0-9]*: complexity report: total:" | sed 's/.*Agents [0-9][0-9]*,//g' | sed 's/\([0-9][0-9]*\).*distinct msg: \([0-9][0-9]]*\)/\1,\2/g' > temp.txt
+  join -t , "${output}_entavgmsg.csv" temp.txt > tmp && mv tmp "${output}_entavgmsg.csv"
+  anum=$[$anum+1]
+  # cat temp.txt
+  # cat "${output}_entavgmsg.csv"
+done
+
+cat "${output}_entavgmsg.csv"
 rm temp.txt
